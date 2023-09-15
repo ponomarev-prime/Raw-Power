cf5d8eb23be711a0c94c3f79e5bff8e260d48b03
Alex <ax.dev@yahoo.com>
2023-09-16 02:05:36 +0300

    args

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func main() {
 	argsWithProg := os.Args
 	argsWithoutProg := os.Args[1:]
 
-	//arg := os.Args[3]
+	arg := os.Args[3]
 
 	fmt.Println(argsWithProg)
 	fmt.Println(argsWithoutProg)
-	//fmt.Println(arg)
+	fmt.Println(arg)
 }
