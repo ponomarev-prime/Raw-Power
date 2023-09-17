5624fc4419ec6d37cf927582836a10de0982230c
Alex <ax.dev@yahoo.com>
2023-09-17 12:33:52 +0300

    args type 3, default args

--- main.go
+++ main.go
@@ -6,15 +6,24 @@ import (
 	"strings"
 )
 
+type CmdConfigs struct {
+	fileName string
+	flagX    bool
+}
+
+// Значения по умолчанию
+var defaultCmdConfigs = CmdConfigs{
+	fileName: "default.txt",
+	flagX:    false,
+}
+
 func main() {
 	fmt.Println("begin")
 
-	printArgs := getSysArgs()
-	fmt.Println(printArgs)
+	fmt.Println(getCmdConfigs(getSysArgs()))
 
 	stringArray := []string{"Hello", "world", "!", "!!"}
-	printArrayJoined := arrayElementJoiner(stringArray)
-	fmt.Println(printArrayJoined)
+	fmt.Println(arrayElementJoiner(stringArray))
 }
 
 func getSysArgs() []string {
@@ -25,4 +34,19 @@ func getSysArgs() []string {
 func arrayElementJoiner(stringArray []string) string {
 	justString := strings.Join(stringArray, " ")
 	return justString
+}
+
+func getCmdConfigs(sysArgs []string) CmdConfigs {
+	if len(sysArgs) == 0 {
+		fmt.Println("Zero args")
+		CmdConfigs := defaultCmdConfigs
+		return CmdConfigs
+	}
+
+	partName := strings.Split(sysArgs[0], "=")[1]
+
+	CmdConfigs := defaultCmdConfigs
+	CmdConfigs.fileName = partName
+	CmdConfigs.flagX = true
+	return CmdConfigs
 }
